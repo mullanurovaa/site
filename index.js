d3080ac02850fd9f59a052d6db990b5b3fa24184
mullanurovaa <mullanurova99@gmail.com>
2021-12-07 16:39:52 +0500

    hm1

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-'use strict'
-const product = 'молоко'
-const price = '500'
-let text = `${product}, цена ${price} рублей.`
-alert(text)
+'use strict';
+const product = 'молоко';
+const price = '500';
+const text = `${product}, цена ${price} рублей.`;
+alert(text);
